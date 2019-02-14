f61e54c9c44d171efa854320962dfaff38cffcf7
Laura Gillespie <lauragillespie@Lauras-MacBook-Pro.local>
2019-02-14 17:39:26 -0500

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("Cheeseburger");
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("Bacon Burger");
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("Veggie Burger");
